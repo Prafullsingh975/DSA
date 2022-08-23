100b5f978133df8fd30e6ecb01425d88192c421d
Prafull Singh <prafullsingh975@gmail.com>
2022-08-23 22:12:17 +0530

    LCM

--- LCM.java
+++ LCM.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class LCM {
+import java.util.Scanner;
+
+public class LCM {
+    static int gcd(int a, int b){
+        while (a!=0){
+            int gcd = b%a;
+            b = a;
+            a = gcd;
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int gcd = gcd(num1,num2);
+        int lcm = (num1*num2)/gcd;
+        System.out.println("LCM("+num1+","+num2+") = "+lcm);
+    }
 }
